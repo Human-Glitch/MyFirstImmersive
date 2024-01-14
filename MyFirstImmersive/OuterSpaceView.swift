9a2b275bb596ab621c1a088946055ed67c0f28ed
Kody Buss <kody.buss@gmail.com>
2024-01-13 19:28:20 -0600

    Refactor for manageability

--- MyFirstImmersive/OuterSpaceView.swift
+++ MyFirstImmersive/OuterSpaceView.swift
@@ -9,60 +9,75 @@ import SwiftUI
 import RealityKit
 import RealityKitContent
 
+// Component for the toggle switch with glass effect
+struct ImmersiveToggle: View {
+	@Binding var isOn: Bool
+
+	var body: some View {
+		Toggle("Immersive View", isOn: $isOn)
+			.font(.callout)
+			.frame(width: 175, alignment: .center)
+			.padding()
+			.glassBackgroundEffect()
+	}
+}
+
+// Main View
 struct OuterSpaceView: View {
-	@State private var showImmersiveSpace = false
-	@State private var immersiveSpaceIsShown = false
+	@State private var isImmersiveSpaceShown = false
 
 	@Environment(\.openImmersiveSpace) var openImmersiveSpace
 	@Environment(\.dismissImmersiveSpace) var dismissImmersiveSpace
 	
-    var body: some View {
-		
-		ZStack(alignment: Alignment(horizontal: .center, vertical: .top), content: {
-			Image("Earth Space")
-				.resizable()
-				.scaledToFill()
-				.background()
-				.presentationCornerRadius(25)
-			
-			Text("Hello World")
-				.padding(50)
-				.kerning(25)
-				.font(.extraLargeTitle)
-				.fontDesign(.monospaced)
-				.fontWeight(.ultraLight)
-				.ornament(attachmentAnchor: .scene(.bottom)) {
-					VStack{
-						Toggle("Immersive View", isOn: $showImmersiveSpace)
-							.font(.callout)
-					}
-					.frame(width: 175, alignment: .bottom)
-					.padding()
-					.glassBackgroundEffect()
+	var body: some View {
+		ZStack(alignment: .top) {
+			BackgroundImageView()
+			HelloWorldView(isImmersiveSpaceShown: $isImmersiveSpaceShown)
+		}
+		.glassBackgroundEffect()
+		.onChange(of: isImmersiveSpaceShown) { _, newValue in
+			handleImmersiveSpaceChange(isShown: newValue)
+		}
+	}
+
+	private func BackgroundImageView() -> some View {
+		Image("Earth Space")
+			.resizable()
+			.aspectRatio(contentMode: .fit)
+	}
+
+	private func HelloWorldView(isImmersiveSpaceShown: Binding<Bool>) -> some View {
+		Text("Hello World")
+			.padding(50)
+			.kerning(20)
+			.font(.extraLargeTitle)
+			.fontDesign(.monospaced)
+			.fontWeight(.ultraLight)
+			.ornament(attachmentAnchor: .scene(.bottom)) {
+				ImmersiveToggle(isOn: isImmersiveSpaceShown)
+			}
+	}
+
+	// Handle immersive space state change
+	private func handleImmersiveSpaceChange(isShown: Bool) {
+		Task {
+			if isShown {
+				switch await openImmersiveSpace(id: "ImmersiveSpace") {
+				case .opened:
+					isImmersiveSpaceShown = true
+				case .error, .userCancelled:
+					fallthrough
+				@unknown default:
+					isImmersiveSpaceShown = false
 				}
-			
-			
-		}).onChange(of: showImmersiveSpace) { _, newValue in
-			Task {
-				if newValue {
-					switch await openImmersiveSpace(id: "ImmersiveSpace") {
-					case .opened:
-						immersiveSpaceIsShown = true
-					case .error, .userCancelled:
-						fallthrough
-					@unknown default:
-						immersiveSpaceIsShown = false
-						showImmersiveSpace = false
-					}
-				} else if immersiveSpaceIsShown {
-					await dismissImmersiveSpace()
-					immersiveSpaceIsShown = false
-				}
+			} else {
+				await dismissImmersiveSpace()
+				isImmersiveSpaceShown = false
 			}
 		}
-    }
+	}
 }
 
-#Preview (windowStyle: .volumetric){
-    OuterSpaceView()
+#Preview{
+	OuterSpaceView()
 }
